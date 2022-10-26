4c3ef927cb4703e175fde52db466251f1d6708a6
Thomas Lamiable <lamiablethomas@gmail.com>
2022-10-26 14:05:43 +0200

    Global clear for good repo

--- Flutter_app_horse/android/app/src/main/kotlin/com/example/flutter_app_horse/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.flutter_app_horse
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- Flutter_app_horse/lib/mango/constantMango.dart
+++
@@ -1,2 +0,0 @@
-const MONGO_URL="mongodb+srv://admin:1234@cluster0.ovj47rl.mongodb.net/test?retryWrites=true&w=majority";
-const COLLECTION_NAME="users";
