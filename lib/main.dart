dd4ada5988b0fc154460c6872b6ab7ca66966521
Thomas Lamiable <lamiablethomas@gmail.com>
2022-10-27 09:34:39 +0200

    FEATURE - Use Main for menu and create another page for route

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_app_horse/register.dart';
-import 'Mango/MangoDB.dart';
+import 'package:flutter_app/Mango/MangoDB.dart';
+import 'package:flutter_app/register.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -10,12 +10,23 @@ Future<void> main() async {
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
+  static const tag = "Register_page";
 
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // Try running your application with "flutter run". You'll see the
+        // application has a blue toolbar. Then, without quitting the app, try
+        // changing the primarySwatch below to Colors.green and then invoke
+        // "hot reload" (press "r" in the console where you ran "flutter run",
+        // or simply save your changes to "hot reload" in a Flutter IDE).
+        // Notice that the counter didn't reset back to zero; the application
+        // is not restarted.
         primarySwatch: Colors.blue,
       ),
       routes: {RegisterPage.tag:(context)=>RegisterPage(title: 'Register')},
@@ -27,6 +38,15 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
 
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
+
   final String title;
 
   @override
@@ -34,7 +54,6 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-
   @override
   Widget build(BuildContext context) {
     {
@@ -42,137 +61,14 @@ class _MyHomePageState extends State<MyHomePage> {
           backgroundColor: Colors.white,
           body: SingleChildScrollView(
             child: Container(
-              child: Column(
-                children: <Widget>[
-                  Container(
-                    height: 400,
-                    decoration: BoxDecoration(
-                    ),
-                    child: Stack(
-                      children: <Widget>[
-                        Positioned(
-                          left: 30,
-                          width: 80,
-                          height: 200,
-                          child: (Container(
-                            decoration: BoxDecoration(
-                            ),
-                          )),
-                        ),
-                        Positioned(
-                          left: 140,
-                          width: 80,
-                          height: 150,
-                          child: (Container(
-                            decoration: BoxDecoration(
-
-                            ),
-                          )),
-                        ),
-                        Positioned(
-                          right: 40,
-                          top: 40,
-                          width: 80,
-                          height: 150,
-                          child: (Container(
-                            decoration: BoxDecoration(
-                            ),
-                          )),
-                        ),
-                        Positioned(
-                          child: (Container(
-                            margin: EdgeInsets.only(top: 50),
-                            child: Center(
-                              child: Text("Login", style: TextStyle(
-                                  color: Colors.white,
-                                  fontSize: 40,
-                                  fontWeight: FontWeight.bold),),
-                            ),
-                          )),
-                        )
-                      ],
-                    ),
-                  ),
-                  Padding(
-                    padding: EdgeInsets.all(30.0),
-                    child: Column(
-                      children: <Widget>[
-                        (Container(
-                          padding: EdgeInsets.all(5),
-                          decoration: BoxDecoration(
-                              color: Colors.white,
-                              borderRadius: BorderRadius.circular(10),
-                              boxShadow: [
-                                BoxShadow(
-                                    color: Color.fromRGBO(143, 148, 251, .2),
-                                    blurRadius: 20.0,
-                                    offset: Offset(0, 10)
-                                )
-                              ]
-                          ),
-                          child: Column(
-                            children: <Widget>[
-                              Container(
-                                padding: EdgeInsets.all(8.0),
-                                decoration: BoxDecoration(
-                                    border: Border(bottom: BorderSide(
-                                        color: Colors.grey))
-                                ),
-                                child: TextField(
-                                  decoration: InputDecoration(
-                                      border: InputBorder.none,
-                                      hintText: "Email or Phone number",
-                                      hintStyle: TextStyle(
-                                          color: Colors.grey[400])
-                                  ),
-                                ),
-                              ),
-                              Container(
-                                padding: EdgeInsets.all(8.0),
-                                child: TextField(
-                                  decoration: InputDecoration(
-                                      border: InputBorder.none,
-                                      hintText: "Password",
-                                      hintStyle: TextStyle(
-                                          color: Colors.grey[400])
-                                  ),
-                                ),
-                              )
-                            ],
-                          ),
-                        )),
-                        SizedBox(height: 30,),
-                        (Container(
-                          height: 50,
-                          decoration: BoxDecoration(
-                              borderRadius: BorderRadius.circular(10),
-                              gradient: LinearGradient(
-                                  colors: [
-                                    Color.fromRGBO(143, 148, 251, 1),
-                                    Color.fromRGBO(143, 148, 251, .6),
-                                  ]
-                              )
-                          ),
-                          child: Center(
-                           child: ElevatedButton(
-         onPressed: (){
-           Navigator.of(context).pushNamed(RegisterPage.tag);
-           },
-         child: Text("Register"),
-                           
-                        )),
-                        SizedBox(height: 70,),
-                        (Text(
-                          "Forgot Password?", style: TextStyle(color: Color
-                            .fromRGBO(143, 148, 251, 1)),)),
-                      ],
-                    ),
-                  )
-                ],
+              child: ElevatedButton(onPressed: () {
+                Navigator.of(context).pushNamed(RegisterPage.tag);
+              }, child: Text("Salut"),
+                
               ),
+              
             ),
-          )
-      );
+          ));
     }
   }
 }
